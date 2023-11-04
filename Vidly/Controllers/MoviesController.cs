1509ab3d378d245b6c7f3eda0aad9119cba7db04
sreejanc <4500sreejanchowdhury@gmail.com>
2023-11-04 11:57:25 +0530

    1st Commit

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -15,5 +15,24 @@ namespace Vidly.Controllers
             var movie = new Movie() { Name = "Shrek" };
             return View(movie);
         }
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+        /// <summary>
+        /// Demonstration of what happens if Action Parameters are optional field. 
+        /// </summary>
+        /// <param name="pageIndex"></param>
+        /// <param name="sortBy"></param>
+        /// <returns>Content()</returns>
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex = {0} & sortBy = {1}", pageIndex, sortBy));
+        }
+
     }
 }
