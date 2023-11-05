71d474c6c04f50b8dfbdf795a8b2030124fa99d7
sreejanc <4500sreejanchowdhury@gmail.com>
2023-11-05 08:00:01 +0530

    Section 2 Completed!

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,17 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Random()
+        public ActionResult Index()
         {
-            var movie = new Movie() { Name = "Shrek" };
+            var movie = GetMovies();
             return View(movie);
         }
+
         public ActionResult Edit(int id)
         {
             return Content("id = " + id);
@@ -25,14 +27,27 @@ namespace Vidly.Controllers
         /// <param name="pageIndex"></param>
         /// <param name="sortBy"></param>
         /// <returns>Content()</returns>
-        public ActionResult Index(int? pageIndex, string sortBy)
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (string.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+        //    return Content(String.Format("pageIndex = {0} & sortBy = {1}", pageIndex, sortBy));
+        //}
+        
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
         {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-            return Content(String.Format("pageIndex = {0} & sortBy = {1}", pageIndex, sortBy));
+            return Content(year + "/" + month);
         }
-
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek!"},
+                new Movie { Id = 2, Name = "Wall-e"}
+            };
+        }
     }
 }
